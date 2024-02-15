c4ed684191696a56375246cc8efe9f38e92b819a
Bekzhan <bekzhan911@gmail.com>
2024-02-15 12:49:42 +0600

    Some lecture examples

---
+++ lecture1-examples/example1.py
@@ -0,0 +1,17 @@
+#name = input()
+#print("Hello" + " " + name, end=" ")
+#print("Hello", name)
+#print("Hello \n World")
+
+#int 
+#float
+#complex
+
+z=1
+y=1.5
+x=(1+2j)
+print(z+y+x)
+
+str1="Hello World"
+char1='c'
+print(type(str1),type(char1))
